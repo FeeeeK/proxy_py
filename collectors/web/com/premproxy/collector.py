4e56a82de6f4b85b7dac1044a5e1bcdcad3c9e66
FeeeeK <26704473+FeeeeK@users.noreply.github.com>
2021-09-27 20:54:30 +0300

    .

--- collectors/web/com/premproxy/collector.py
+++ collectors/web/com/premproxy/collector.py
@@ -67,5 +67,5 @@ class CollectorSocksList(BaseCollectorPremProxyCom):
 
     def __init__(self):
         super(CollectorSocksList, self).__init__(
-            "https://premproxy.com/socks-list/", 17
+            "https://premproxy.com/socks-list/", 16
         )
